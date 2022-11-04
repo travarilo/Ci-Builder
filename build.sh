702cf834a98114603d4091aa40ea94ab2b619860
IQ7 <111713272+IQ-7@users.noreply.github.com>
2022-11-04 22:04:06 +0700

    whyred: build: ForkLineageOS 12.1

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve-plus -g default,-mips,-darwin,-notdefault
-git clone https://github.com/IQ-7/local_manifest --depth 1 -b banana-13 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ForkLineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/IQ-7/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -13,12 +13,12 @@ export KBUILD_BUILD_USER=IQ-7
 export KBUILD_BUILD_HOST=Corp
 export BUILD_USERNAME=IQ-7
 export BUILD_HOSTNAME=Corp
-lunch banana_whyred-userdebug
+lunch lineage_whyred-userdebug
 mkfifo reading # Jangan di Hapus
 tee "${BUILDLOG}" < reading & # Jangan di Hapus
 build_message "Building Started" # Jangan di Hapus
 progress & # Jangan di Hapus
-mka banana -j8  > reading # Jangan di hapus text line (> reading)
+make bacon -j8  > reading # Jangan di hapus text line (> reading)
 
 retVal=$?
 timeEnd
