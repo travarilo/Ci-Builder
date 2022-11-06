3599d77e2e1767af5ced4d5933c711aa1e840ed2
IQ7 <111713272+IQ-7@users.noreply.github.com>
2022-11-06 18:20:40 +0700

    build: BananaDroid A12L

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/IQ-7/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -13,7 +13,7 @@ export KBUILD_BUILD_USER=IQ-7
 export KBUILD_BUILD_HOST=Corp
 export BUILD_USERNAME=IQ-7
 export BUILD_HOSTNAME=Corp
-lunch lineage_whyred-userdebug
+lunch banana_whyred-userdebug
 mkfifo reading # Jangan di Hapus
 tee "${BUILDLOG}" < reading & # Jangan di Hapus
 build_message "Building Started" # Jangan di Hapus
@@ -23,4 +23,4 @@ make bacon -j8  > reading # Jangan di hapus text line (> reading)
 retVal=$?
 timeEnd
 statusBuild
-# Build LineageOS 19.1
+# Build BananaDroid A12L
